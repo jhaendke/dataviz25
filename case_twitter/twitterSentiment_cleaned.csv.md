0a327a482f182a6b83e4c75992001816c46f888e
Jannis Haendke <7644955+jhaendke@users.noreply.github.com>
2025-02-04 16:19:01 +0100

    finished cleaning

---
+++ case_twitter/twitterSentiment_cleaned.csv.md
@@ -0,0 +1,1 @@
+See Google Drive for full cleaned dataset.
--- chart1.R
+++ chart1.R
@@ -2,40 +2,93 @@
 # getwd()
 # setwd("")
 
+# install.packages("")
+
 # Environment
 # Sys.getenv("VAR1")
 
 library(tidyverse)
 library(ggplot2)
+library(lubridate)
 #library(ggthemes)
 
 ### Dataset 2 "Twitter"
 twitter <- read.csv("case_twitter/twitterSentiment.csv")
-head(twitter)
+#head(twitter)
 colnames(twitter)
+view(head(twitter))
 
-# Cleaning
-# Drop cols
+### Cleaning
+
+# Drop & Rename cols, rows
 twitter <- twitter %>% 
-    select(-User.Name)
-    #select(timestamp)
-    #filter(date != "date")
-# Write twitter to CSV
-write.csv2(twitter, "case_twitter/twitterSentiment_cleaned.csv", row.names = FALSE)
+    select(-X,-Negative,-Neutral,-Positive) %>%
+    rename(user=User.Name,text=Text,retweet=Retweet.Count,favorite=Favorite.Count,date=Date)
+    #slice(1:1000)
+    #filter()
 
+# Anonymize usernames
+unique_user <- unique(twitter$user)
+user_to_id <- setNames(seq_along(unique_user), unique_user)
+twitter$user_id <- user_to_id[twitter$user] # add col
+user_id_table <- data.frame(user = names(user_to_id), id = user_to_id, row.names = NULL) # create mapping table
+twitter <- twitter %>% 
+    select(-user)
+
+view(head(user_id_table))
+view(head(twitter))
+
+# Transform var types
+
+# date: 692 unique dates in twitter
+length(unique(twitter$date))
+
+# sentiment: 3 levels
+twitter <- twitter %>% 
+    mutate(sentiment = as.factor(sentiment))
+unique(twitter$sentiment)
+levels(twitter$sentiment)
+
+# date
+twitter$date <- as.Date(twitter$date, format = "%m/%d/%Y")
+ #timestamp_new <- as.POSIXct(timestamp, format="%Y-%m-%dT%H:%M:%OSZ", tz="CET")
+
+### Write twitter to CSV
+write.csv2(twitter, "case_twitter/twitterSentiment_cleaned.csv", row.names = FALSE)
 # Write shorter dataset
 twitter_short <- twitter %>%
-    slice(1:5000)
+    slice(1:3000)
 write.csv2(twitter_short, "case_twitter/twitterSentiment_cleaned_short.csv", row.names = FALSE)
 
+### Analysis
+
+# Sentiment by date
+
+# 1) insert col weekday
+#    insert col month
+twitter <- twitter %>% 
+    mutate(
+        weekday = wday(date, label = TRUE),
+        month = month(date, label = TRUE)
+    )
 
 
 
-# Clean timestamp
-# Convert ISO 8601 to POSIXct & CET timezone
-#timestamp <- "2024-08-05T12:15:21.193Z"
-#posix_timestamp <- as.POSIXct(timestamp, format="%Y-%m-%dT%H:%M:%OSZ", tz="CET")
-#print(posix_timestamp)
-#
-# convert all timestamps
-#df$timestamp_CET <- as.POSIXct(df$timestamp, format="%Y-%m-%dT%H:%M:%OSZ", tz="CET")
+
+# 2) basic stats
+
+# ) count sentiment (pos/neg/neut) per date
+
+# ) plot 
+
+# deviation from avg per month (bipolar)
+
+# boxplot per month
+
+# pie
+
+# Steamgraph
+
+# stacked grouped bars
+
+# ) LM day_of_month <> sentiment
